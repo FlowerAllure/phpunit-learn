3c084a920282a0fc243ec823d043b1f504c6054c
feng.pan <feng.pan@mail.com>
2021-12-14 17:43:44 +0800

    first commit

---
+++ src/MyTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Flowerallure\PhpunitLearn;
+
+use PHPUnit\Framework\TestCase;
+
+class MyTest extends TestCase
+{
+    public function testOne()
+    {
+        $this->assertTrue(true);
+    }
+}
